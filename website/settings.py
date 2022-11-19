f12d6f02f7cdeb4fd311e964d64473d13f0a8ce7
purnasandeep <purnasandeepb@gmail.com>
2022-11-19 19:28:11 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e7n^ve*g_k-ea-$5omncnsl-$zzal1uaicej(uu^5=p-_pcoz4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','pricetracker-2.herokuapp.com']
 
 
 # Application definition
